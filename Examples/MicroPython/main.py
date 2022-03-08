86d1855c40bb61411ad3cb8c78ea149ff6a7ac3e
John F Hoeppner <johnh.abbykus@gmail.com>
2022-03-08 10:12:05 +0700

    Add files via upload

--- Examples/MicroPython/main.py
+++ Examples/MicroPython/main.py
@@ -1,7 +1,7 @@
 import machine
 import time
-pin = machine.Pin(2, machine.Pin.OUT)
-for i in range(20):
+pin = machine.Pin(2, machine.Pin.OUT) // GPIO 2 is the ESP-12F LED
+for i in range(30):
    pin.off()
    time.sleep(0.5)
    pin.on()
